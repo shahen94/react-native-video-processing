493274b099e7d4d0d5d48a5ef8419155f2be70d3
hhravn <hhravn@gmail.com>
2018-04-10 13:35:54 +0200

    reindent

--- android/src/main/java/com/shahenlibrary/Trimmer/TrimmerManager.java
+++ android/src/main/java/com/shahenlibrary/Trimmer/TrimmerManager.java
@@ -36,104 +36,104 @@ import com.facebook.react.common.MapBuilder;
 import java.util.Map;
 
 public class TrimmerManager extends ReactContextBaseJavaModule {
-    static final String REACT_PACKAGE = "RNTrimmerManager";
+  static final String REACT_PACKAGE = "RNTrimmerManager";
 
-    private final ReactApplicationContext reactContext;
+  private final ReactApplicationContext reactContext;
 
-    public TrimmerManager(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
-        loadFfmpeg();
+  public TrimmerManager(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+    loadFfmpeg();
+  }
+
+  @Override
+  public String getName() {
+    return REACT_PACKAGE;
+  }
+
+  @ReactMethod
+  public void getPreviewImages(String path, Promise promise) {
+    try {
+      Log.d(REACT_PACKAGE, "getPreviewImages: " + path);
+      Trimmer.getPreviewImages(path, promise, reactContext);
+    } catch (Throwable t) {
+      promise.reject(t);
     }
+  }
 
-    @Override
-    public String getName() {
-        return REACT_PACKAGE;
+  @ReactMethod
+  public void getVideoInfo(String path, Promise promise) {
+    try {
+      Log.d(REACT_PACKAGE, "getVideoInfo: " + path);
+      Trimmer.getVideoInfo(path, promise, reactContext);
+    } catch (Throwable t) {
+      promise.reject(t);
     }
+  }
 
-    @ReactMethod
-    public void getPreviewImages(String path, Promise promise) {
-        try {
-            Log.d(REACT_PACKAGE, "getPreviewImages: " + path);
-            Trimmer.getPreviewImages(path, promise, reactContext);
-        } catch (Throwable t) {
-            promise.reject(t);
-        }
+  @ReactMethod
+  public void trim(ReadableMap options, Promise promise) {
+    try {
+      Log.d(REACT_PACKAGE, options.toString());
+      Trimmer.trim(options, promise, reactContext);
+    } catch (Throwable t) {
+      promise.reject(t);
     }
+  }
 
-    @ReactMethod
-    public void getVideoInfo(String path, Promise promise) {
-        try {
-            Log.d(REACT_PACKAGE, "getVideoInfo: " + path);
-            Trimmer.getVideoInfo(path, promise, reactContext);
-        } catch (Throwable t) {
-            promise.reject(t);
-        }
+  @ReactMethod
+  public void compress(String path, ReadableMap options, Promise promise) {
+    try {
+      Log.d(REACT_PACKAGE, "compress video: " + options.toString());
+      Trimmer.compress(path, options, promise, null, null, reactContext);
+    } catch (Throwable t) {
+      promise.reject(t);
     }
+  }
 
-    @ReactMethod
-    public void trim(ReadableMap options, Promise promise) {
-        try {
-            Log.d(REACT_PACKAGE, options.toString());
-            Trimmer.trim(options, promise, reactContext);
-        } catch (Throwable t) {
-            promise.reject(t);
-        }
+  @ReactMethod
+  public void getPreviewImageAtPosition(ReadableMap options, Promise promise) {
+    try {
+      String source = options.getString("source");
+      double sec = options.hasKey("second") ? options.getDouble("second") : 0;
+      String format = options.hasKey("format") ? options.getString("format") : null;
+      Trimmer.getPreviewImageAtPosition(source, sec, format, promise, reactContext);
+    } catch (Throwable t) {
+      promise.reject(t);
     }
+  }
 
-    @ReactMethod
-    public void compress(String path, ReadableMap options, Promise promise) {
-        try {
-            Log.d(REACT_PACKAGE, "compress video: " + options.toString());
-            Trimmer.compress(path, options, promise, null, null, reactContext);
-        } catch (Throwable t) {
-            promise.reject(t);
-        }
+  @ReactMethod
+  public void crop(String path, ReadableMap options, Promise promise) {
+    try {
+      Trimmer.crop(path, options, promise, reactContext);
+    } catch (Throwable t) {
+      promise.reject(t);
     }
+  }
 
-    @ReactMethod
-    public void getPreviewImageAtPosition(ReadableMap options, Promise promise) {
-        try {
-            String source = options.getString("source");
-            double sec = options.hasKey("second") ? options.getDouble("second") : 0;
-            String format = options.hasKey("format") ? options.getString("format") : null;
-            Trimmer.getPreviewImageAtPosition(source, sec, format, promise, reactContext);
-        } catch (Throwable t) {
-            promise.reject(t);
-        }
+  @ReactMethod
+  public void boomerang(String path, Promise promise) {
+    try {
+      Log.d(REACT_PACKAGE, "boomerang video: " + path);
+      Trimmer.boomerang(path, promise, reactContext);
+    } catch (Throwable t) {
+      promise.reject(t);
     }
+  }
 
-    @ReactMethod
-    public void crop(String path, ReadableMap options, Promise promise) {
-        try {
-            Trimmer.crop(path, options, promise, reactContext);
-        } catch (Throwable t) {
-            promise.reject(t);
-        }
+  @ReactMethod
+  public void reverse(String path, Promise promise) {
+    try {
+      Log.d(REACT_PACKAGE, "reverse video: " + path);
+      Trimmer.reverse(path, promise, reactContext);
+    } catch (Throwable t) {
+      promise.reject(t);
     }
+  }
 
-    @ReactMethod
-    public void boomerang(String path, Promise promise) {
-        try {
-            Log.d(REACT_PACKAGE, "boomerang video: " + path);
-            Trimmer.boomerang(path, promise, reactContext);
-        } catch (Throwable t) {
-            promise.reject(t);
-        }
-    }
-
-    @ReactMethod
-    public void reverse(String path, Promise promise) {
-        try {
-            Log.d(REACT_PACKAGE, "reverse video: " + path);
-            Trimmer.reverse(path, promise, reactContext);
-        } catch (Throwable t) {
-            promise.reject(t);
-        }
-    }
-
-    @ReactMethod
-    private void loadFfmpeg() {
-        Trimmer.loadFfmpeg(reactContext);
-    }
+  @ReactMethod
+  private void loadFfmpeg() {
+    Trimmer.loadFfmpeg(reactContext);
+  }
 }
