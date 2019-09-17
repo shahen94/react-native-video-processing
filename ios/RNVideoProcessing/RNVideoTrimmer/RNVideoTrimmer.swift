8a161dfabaa1dd370cfcb737321c90a4f87c5a48
Eduardo Ferreira <edferreira92@gmail.com>
2019-09-17 10:51:39 -0300

    require queue main warning resolved

--- ios/RNVideoProcessing/RNVideoTrimmer/RNVideoTrimmer.swift
+++ ios/RNVideoProcessing/RNVideoTrimmer/RNVideoTrimmer.swift
@@ -21,6 +21,9 @@ enum QUALITY_ENUM: String {
 
 @objc(RNVideoTrimmer)
 class RNVideoTrimmer: NSObject {
+  @objc class func requiresMainQueueSetup() -> Bool {
+    return true
+  }
 
   @objc func getVideoOrientationFromAsset(asset : AVAsset) -> UIImage.Orientation {
     let videoTrack: AVAssetTrack? = asset.tracks(withMediaType: .video)[0]
