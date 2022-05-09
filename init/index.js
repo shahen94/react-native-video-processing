2c03af1e2623ef0302e900773d15ee042409a28f
Shahen Hovhannisyan <shahen.hovhannisyan.94@gmail.com>
2022-05-09 19:43:45 +0400

    chore(legacy): minor cleanup

--- init/index.js
+++
@@ -1,7 +0,0 @@
-const createLibrary = require('react-native-create-library');
-
-createLibrary({
-	name: 'VideoProcessing',
-	platforms: ['ios', 'android'],
-	packageIdentifier: 'com.shahenlibrary'
-});
